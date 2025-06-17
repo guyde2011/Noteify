1fae24b0c8e5ce5333aa4a118707cd4ba7d59c7a
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-06-17 14:48:58 +0300

    removed old code that was for markdown

--- src/symbolDoc.ts
+++ src/symbolDoc.ts
@@ -1,5 +1,4 @@
 import { Uri } from "vscode";
-import { DocsManager as DocManager, DocSection, MarkdownSection } from "./backends/localFiles";
 
 export type Symbol = string;
 
@@ -10,102 +9,6 @@ export abstract class LinkedDoc {
     constructor() {
         this.innerId = linkedDocId++;
     }
-}
-
-export class SymbolDoc extends LinkedDoc {
-    constructor(
-        public symbol: Symbol,
-        public docs: DocSection,
-        public uri: Uri,
-        private readonly manager: SymbolDocManager,
-    ) {
-        super();
-    }
-
-    sendUpdate(): void {
-        this.manager.updateDocs(this.uri);
-    }
-};
-
-
-export class SymbolDocManager {
-    // TODO: Maybe make the element type a map index by innerId for faster dedup
-    private symbolDocs: Map<Symbol, SymbolDoc[]>;
-
-    constructor(public readonly docManager: DocManager) {
-        this.symbolDocs = new Map<Symbol, SymbolDoc[]>();
-    }
-
-    private extractSymbolDocs(sections: DocSection[], uri: Uri): SymbolDoc[] {
-        return sections.filter((section) => isSymbolLike(section.section.title)).map((section) => {
-            return new SymbolDoc(
-                section.section.title.trim(),
-                section,
-                uri,
-                this
-            );
-        });
-    }
-
-    private processDocs(sections: DocSection[], docUri: Uri): SymbolDoc[] {
-        // TODO: prevent duplication processing of symbol docs
-        let output = [];
-        for (const symbolDoc of this.extractSymbolDocs(sections, docUri)) {
-            if (!this.symbolDocs.has(symbolDoc.symbol)) {
-                this.symbolDocs.set(symbolDoc.symbol, []);
-            }
-            let newDoc = true;
-            for (const doc of this.symbolDocs.get(symbolDoc.symbol)!) {
-                if (doc.innerId === symbolDoc.innerId) {
-                    // Workaround
-                    newDoc = false;
-                    break;
-                }
-                if (doc.uri === docUri && doc.docs.sectionIndex === symbolDoc.docs.sectionIndex) {
-                    doc.docs = symbolDoc.docs;
-                    newDoc = false;
-                    break;
-                }
-            }
-
-            if (newDoc) {
-                output.push(symbolDoc);
-                this.symbolDocs.get(symbolDoc.symbol)!.push(symbolDoc);
-            }
-        }
-        return output;
-    }
-
-    getSymbolDocs(symbol: Symbol): SymbolDoc[] {
-        if (this.symbolDocs.has(symbol)) {
-            return this.symbolDocs.get(symbol)!;
-        }
-        return [];
-    }
-
-
-    getAllSymbols(): Symbol[] {
-        return Array.from(this.symbolDocs.keys());
-    }
-
-
-    updateDocs(uri: Uri): SymbolDoc[] {
-        // TODO: FFS stop updating every comment for every change!
-        const sections = this.docManager.getSections(uri);
-        if (!sections) {
-            return [];
-        }
-        return this.processDocs(sections, uri);
-    }
-
-    createSymbolDoc(symbol: Symbol, docUri: Uri): SymbolDoc {
-        // TODO: figure the right hashCount
-        const emptySection = new MarkdownSection(` ${symbol}`, 3, [""], this.docManager);
-        this.docManager.appendSection(docUri, emptySection);
-        const updatedDocs = this.updateDocs(docUri);
-        return updatedDocs[updatedDocs.length - 1];
-    }
-
 }
 
 // export var symbolDocManager = new SymbolDocManager(docManager);
