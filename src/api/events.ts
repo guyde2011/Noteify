19cca53d1de705e25f2f56ad5a876b57b5b2c846
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-06-19 15:28:14 +0300

    organizing code

--- src/api/events.ts
+++ src/api/events.ts
@@ -1,3 +1,7 @@
+/**
+ * This file defines the Session-Backend interface. Other messages may exist within backends, but they are not processed by the Session.
+ */
+
 import * as Doc from "./document";
 import { BackendInstance } from "./interface";
 
