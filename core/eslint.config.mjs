b1b17af2c20f38ddd67f06cbad3075260468cfaa
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-06-24 17:51:30 +0300

    initial shared core library

---
+++ core/eslint.config.mjs
@@ -0,0 +1,55 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommend from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    eslintPluginPrettierRecommend,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/require-await": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-require-imports": [
+                "error",
+                {
+                    allow: [],
+                    allowAsImport: true,
+                },
+            ],
+            "@typescript-eslint/no-unnecessary-condition": [
+                "error",
+                { allowConstantLoopConditions: true },
+            ],
+            "@typescript-eslint/restrict-template-expressions": [
+                "error",
+                { allowNumber: true },
+            ],
+        },
+    },
+    {
+        files: ["eslint.config.mjs"],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+);
